da3aa1494509b286e355baadd79efc9b7ead3696
illicchpv <illich58@mail.ru>
2023-10-24 12:06:20 +0300

    schedule и directions примерно

--- js/IncludHtml.js
+++ js/IncludHtml.js
@@ -4,11 +4,10 @@ let IncludHtml = (function () {
   let _incs_count = 0;
   let _finish_callback = false;
   let _defProps = false;
+  let _selectorClass = 'incs'
 
-  function replaceAll(src, search, replace) {
-    return src.split(search).join(replace);
-  }
   function doIncludAll( selectorClass, defProps, finish_callback = false){
+    _selectorClass = selectorClass;
     _defProps = defProps;
     _finish_callback = finish_callback;
     const incs = document.querySelectorAll('.'+selectorClass);
@@ -22,86 +21,89 @@ let IncludHtml = (function () {
     }
     try {
       incs.forEach((el) => {
-        let params = el.dataset.incs
-        el.classList.remove(selectorClass);
-        el.removeAttribute('data-incs');
-        if(!params){
-          console.error("IncludHtml - нет json параметров");
-          return
-        }
-        try{
-          params = JSON.parse(params)
-        }catch(e){
-          console.error("Не удалось разобрать параметры!", e, "data-incs=\r\n", params)
-        }
-        let incFromId = false;
-        incFromId = (_defProps && _defProps.incFromId) ? _defProps.incFromId : incFromId;
-        incFromId = (params && params.incFromId) ? params.incFromId : incFromId;
-        let errSt = !params;
-        errSt = errSt || !incFromId
-        if(!errSt){
-          // params.incFromId
-          // params.incFile
-          // params.onLoadCalback
-          params.docEl = el
-          params.extEl = null
-          params.extUrl = null
-          if(!params.incFile){ // вставка элемента из текущего документа
-            const docElement = document.getElementById(incFromId);
-            if (docElement) {
-              const extEl = docElement.cloneNode(true);
-              extEl.removeAttribute('id');
-              params.extEl = extEl
-              doProcess(params);
-            } else {
-              console.error("IncludHtml - не найден элемент с указанным id:", incFromId);
-            }
-          } else {  // вставка элемента из документа внешнего html файла
-            const url = params.incFile
-            if(!url){
-              console.error("IncludHtml - не задана extUrl");
-              return
-            }
-            fetch(url)
-              .then((response) => {
-                if (response.ok) {
-                  return response.text();
-                }
-              })
-              .then((data) => {
-                if (data) {
-                  const parser = new DOMParser(),
-                    content = "text/html",
-                    DOM = parser.parseFromString(data, content);
-                  const extEl = DOM.getElementById(incFromId); // DOM.body.querySelector('.'+pparams.incClass);
-                  if (extEl) {
-                    extEl.removeAttribute('id');
-                    params.extEl = extEl
-                    doProcess(params);
-                  } else {
-                    console.error("Не найден элемент с id: " + incFromId + "\r\nВ файле: ", url);
-                  }
-                }
-              })
-              .catch((error) => {
-                console.error("Fetch error: ", error);
-              })
-            ;
-          }
-        }
+        doIncludSingle(el)
       });
     } finally {
       setTimeout(()=>{
-        const incs = document.querySelectorAll('.'+selectorClass);
-        if(incs.length > 0){
-          console.error("Рекурсивное вставление элементов пока не отлажено")
-          // debugger;
-        }
+        // const incs = document.querySelectorAll('.'+selectorClass);
+        // if(incs.length > 0){
+        //   console.error("Рекурсивное вставление элементов пока не работает")
+        //   // debugger;
+        // }
         if(_finish_callback){
           _finish_callback();
         }
       }, 100);
     }    
+  }
+  function doIncludSingle(el){
+    let params = el.dataset.incs
+    el.classList.remove(_selectorClass);
+    el.removeAttribute('data-incs');
+    if(!params){
+      console.error("IncludHtml - нет json параметров");
+      return
+    }
+    try{
+      params = JSON.parse(params)
+    }catch(e){
+      console.error("Не удалось разобрать параметры!", e, "data-incs=\r\n", params)
+    }
+    let incFromId = false;
+    incFromId = (_defProps && _defProps.incFromId) ? _defProps.incFromId : incFromId;
+    incFromId = (params && params.incFromId) ? params.incFromId : incFromId;
+    // let errSt = !params;
+    // errSt = errSt || !incFromId
+    if(incFromId && params){
+      // params.incFromId
+      // params.incFile
+      // params.onLoadCalback
+      params.docEl = el
+      params.extEl = null
+      params.extUrl = null
+      if(!params.incFile){ // вставка элемента из текущего документа
+        const docElement = document.getElementById(incFromId);
+        if (docElement) {
+          const extEl = docElement.cloneNode(true);
+          extEl.removeAttribute('id');
+          params.extEl = extEl
+          doProcess(params);
+        } else {
+          console.error("IncludHtml - не найден элемент с указанным id:", incFromId);
+        }
+      } else {  // вставка элемента из документа внешнего html файла
+        const url = params.incFile
+        if(!url){
+          console.error("IncludHtml - не задана extUrl");
+          return
+        }
+        fetch(url)
+          .then((response) => {
+            if (response.ok) {
+              return response.text();
+            }
+          })
+          .then((data) => {
+            if (data) {
+              const parser = new DOMParser(),
+                content = "text/html",
+                DOM = parser.parseFromString(data, content);
+              const extEl = DOM.getElementById(incFromId); // DOM.body.querySelector('.'+pparams.incClass);
+              if (extEl) {
+                extEl.removeAttribute('id');
+                params.extEl = extEl
+                doProcess(params);
+              } else {
+                console.error("Не найден элемент с id: " + incFromId + "\r\nВ файле: ", url);
+              }
+            }
+          })
+          .catch((error) => {
+            console.error("Fetch error: ", error);
+          })
+        ;
+      }
+    }
   }
   function doProcess(params) {
     let insertType = '';
@@ -144,6 +146,13 @@ let IncludHtml = (function () {
         console.warn("catch error in call " + cb + "(params)", e);
       }
     }
+
+    const incs = params.extEl.querySelectorAll('.'+ _selectorClass);
+    if(incs.length > 0){
+      console.error("Рекурсивное вставление элементов пока не работает")
+      // debugger;
+    }
+
     if(insertType && insertType === 'append'){
       params.docEl.append(params.extEl)
     } else if(insertType && insertType === 'prepend'){
@@ -158,7 +167,6 @@ let IncludHtml = (function () {
   }
 
   return {
-    replaceAll,
-    doIncludAll,
+    doIncludAll
   };
 })();
